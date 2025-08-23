50b9902426ad91c359930fc720372ba21b9c4110
Rabbani Shaik <72737263+rabbanishaik26@users.noreply.github.com>
2025-08-22 21:09:06 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/cinsecure/',   // 👈 important for GitHub Pages
+  base: '/',    // ✅ use root for custom domains
   build: {
-    outDir: "docs",      // 👈 build output to docs/
+    outDir: "docs",
   },
 })
